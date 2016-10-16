c8baa4e9255a9d0a6d0474782e653a9d9a0229e4
shinriyo <shinriyo@shinriyos-MacBook.local>
2016-10-16 16:10:10 +0900

    localize

---
+++ js/localize.js
@@ -0,0 +1,27 @@
+    (function() {
+      'use strict';
+      var setLang = function(lang) {
+        i18next.use(i18nextXHRBackend).init({
+          lng: lang
+        }, function(err, t) {
+          //i18nextJquery.init(i18next, $);
+          jqueryI18next.init(i18next, $);
+          $("[data-i18n]").localize();
+        });
+      }; 
+
+      // default
+      setLang("ja");
+
+      // pull down
+      $('#menu-loc li').hover(function(){
+          $("ul:not(:animated)", this).slideDown();
+      }, function(){
+          $("ul.child",this).slideUp();
+      });
+
+      // language
+      $('.lang').click(function(){
+        setLang(this.id);
+      });
+    }());
